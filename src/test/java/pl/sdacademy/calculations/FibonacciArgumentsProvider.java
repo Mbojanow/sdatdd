d94a92825042cd6a2f95edc186b54a825ae0660d
Michal Bojanowski <michal.bojanowski@onegini.com>
2019-11-16 15:57:29 +0100

    day1end

---
+++ src/test/java/pl/sdacademy/calculations/FibonacciArgumentsProvider.java
@@ -0,0 +1,4 @@
+package pl.sdacademy.calculations;
+
+public class FibonacciArgumentsProvider {
+}
---
+++ src/test/java/pl/sdacademy/calculations/FibonacciSeriesTest.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.calculations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciSeriesTest {
+
+}
